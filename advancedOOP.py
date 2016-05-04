e350996a998d48df7bb7b37a59b95cb24be82f19
SteveYan <tinggengyan@gmail.com>
2016-05-04 21:19:26 +0800

    add advanced oop

--- advancedOOP.py
+++ advancedOOP.py
@@ -89,3 +89,34 @@ s4.score = 100
 print  s4.score
 
 
+class Student5(object):
+    def __init__(self, name):
+        self.name = name
+        self.a, self.b = 0, 1  # 初始化两个计数器a，b
+
+    def __str__(self):
+        return 'Student5 ' + self.name
+
+    def __iter__(self):
+        return self  # 实例本身就是迭代对象，故返回自己
+
+    def next(self):
+        self.a, self.b = self.b, self.a + self.b  # 计算下一个值
+        if self.a > 100000:  # 退出循环的条件
+            raise StopIteration();
+        return self.a  # 返回下一个值
+
+    def __getitem__(self, n):
+        a, b = 1, 1
+        for x in range(n):
+            a, b = b, a + b
+        return a
+
+
+s5 = Student5('steve')
+print s5
+
+for b in s5:
+    print b
+
+print s5[9]
